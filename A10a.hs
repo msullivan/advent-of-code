62491a2b3b01cf4ea438e8c6f4e280aa7fc84704
Michael Sullivan <sully@msully.net>
2015-12-10 00:32:29 -0500

    Grumble about the monomorphism restriction

--- A10a.hs
+++ A10a.hs
@@ -7,6 +7,9 @@ import qualified Data.Map as Map
 looksay l = concatMap cleanup $ group l
   where cleanup a = [length a, head a]
 
+-- Without the type annotation, this gets inferred as [Integer], and
+-- then answer doesn't typecheck. Turning off the monomorphism
+-- restriction would also fix this. Grumble. Grumble grumble.
 input :: [Int]
 input = [3,1,1,3,3,2,2,1,1,3]
 
