61a31077b5d18a4978f50d31927e915678a0c467
Michael Sullivan <sully@msully.net>
2015-12-12 00:16:09 -0500

    day 12

---
+++ A12a.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import sys, json
+
+def crawl(obj):
+    if isinstance(obj, int) or isinstance(obj, float):
+        return obj
+
+    sum = 0
+    if isinstance(obj, list):
+        for a in obj:
+            sum += crawl(a)
+    if isinstance(obj, dict):
+        for a in obj.values():
+            sum += crawl(a)
+    return sum
+
+def main(args):
+    shit = "\n".join([s.strip() for s in sys.stdin])
+    js = json.loads(shit)
+
+    print(crawl(js))
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
