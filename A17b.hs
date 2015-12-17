67e5dfc006e3c3f84f9c4aa6828597483ba9b840
Michael Sullivan <sully@msully.net>
2015-12-17 01:45:39 -0500

    Cleanup 17b

--- A17b.hs
+++ A17b.hs
@@ -3,13 +3,13 @@ import Data.List.Extra
 import Data.Maybe
 import qualified Data.Map as Map
 
-nus a =
-  let x = minimum $ map length a
-      b = filter (\z -> length z == x) a
-  in length b
+countMinConfigs l =
+  let min = minimum $ map length l
+  in length $ filter (\z -> length z == min) l
 
 solve :: String -> Int
-solve = nus . filter (\x -> sum x == 150) . subsequences . map read . lines
+solve = countMinConfigs . filter (\x -> sum x == 150) . subsequences .
+        map read . lines
 
 answer f = interact $ (++"\n") . show . f
 main = answer solve
