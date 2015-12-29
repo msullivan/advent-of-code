709599113ff93a7babe81c9dc1efb306e3a948ca
Michael Sullivan <sully@msully.net>
2015-12-29 15:59:42 -0600

    Belated checkin of an upgrade to 24. Do a pruning search using a pruning search monad.

--- A24a.hs
+++ A24a.hs
@@ -1,8 +1,33 @@
+-- For Prune
 import Control.Monad
+import Control.Monad.Reader
+import Control.Monad.ListT
+--import Control.Monad.Trans.List
+import Data.List.Class
+
 import Data.List
 import Data.List.Extra
 import Data.Maybe
 import qualified Data.Map as Map
+
+type Prune r a = ListT (Reader (Maybe r)) a
+
+--pick :: [a] -> Prune a
+pick = fromList
+
+prune :: (a -> Bool) -> Prune a ()
+prune f =
+  do v <- lift ask
+     case v of
+       Nothing -> return ()
+       Just v' -> guard $ f v'
+
+runPrune :: Prune a a -> [a]
+runPrune m = run Nothing m
+  where run v m = case runReader (runListT m) v of
+          Nil -> []
+          Cons x xs -> x : run (Just x) xs
+
 
 --partitions :: Eq a => [a] -> [([a], [a])]
 partitions cost xs =
@@ -10,26 +35,22 @@ partitions cost xs =
      guard $ sum l == cost
      return (l, xs \\ l)
 
---partitions3 :: Eq a => [a] -> [([a], [a], [a])]
-partitions3 cost l =
-  do (a, b) <- partitions cost l
-     (b', c) <- partitions cost b
-     return (a, b', c)
-
---search :: [Int] -> Int
+search :: [Int] -> Prune (Int, Int) (Int, Int)
 search packages =
-  do (a, bc) <- partitions cost packages
-     guard $ length a <= 6
+  do (a, bc) <- pick $ partitions cost packages
+     let v = (length a, product a)
+     prune (v<)
      guard $ not $ null $ partitions cost bc
-     return (length a, product a)
+     return v
 
   where cost = sum packages `div` 3
+
 
 --pack :: [Int] -> Int
 pack packages = search packages
 
 --solve :: String -> Int
-solve = pack . map read . lines
+solve = runPrune . pack . map read . lines
 
 answer f = interact $ (++"\n") . show . f
 main = answer solve
