9cdc186618b29efc48e7a1d955b9b549bcba61ab
Michael Sullivan <sully@msully.net>
2016-12-03 00:55:47 -0500

    Hack up some haskell ones as practice

---
+++ 2016/A3a.hs
@@ -0,0 +1,18 @@
+import Data.List
+import Data.List.Extra
+
+-- Originally done in python but I redid it in haskell to get back in
+-- the feel of writing haskell (which I don't think I've done since
+-- last year's AoC).
+
+iread :: String -> Int
+iread = read
+
+check :: [Int] -> Bool
+check x = let [a,b,c] = sort x
+          in a+b > c
+
+load = map iread . words
+
+answer f = interact $ (++"\n") . show . f
+main = answer $ length . filter check . map load . lines
