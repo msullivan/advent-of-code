c6a7feca8f4c70b5c6f846c5cf7445bca818f785
Michael Sullivan <sully@msully.net>
2016-12-09 01:13:27 -0500

    strip cruft out of day 9

--- 2016/A9a.hs
+++ 2016/A9a.hs
@@ -1,29 +1,15 @@
-import Control.Monad
 import Data.List.Extra
 import Data.Maybe
-import qualified Data.Char as C
-import qualified Data.Map as Map
 
 ------
-iread :: String -> Int
-iread = read
-
 answer :: (Show a) => (String -> a) -> IO ()
 answer f = interact $ (++"\n") . show . f
-
-ord0 c = C.ord c - C.ord 'a'
-chr0 i = C.chr (i + C.ord 'a')
-incletter c i = chr0 ((ord0 c + i) `mod` 26)
-
 splitOn1 a b = fromJust $ stripInfix a b
-rsplitOn1 a b = fromJust $ stripInfixEnd a b
-
-
 --------
 
 decompress ('(':xs) =
   let (lol, ys) = splitOn1 ")" xs
-      [n, m] = map iread $ splitOn "x" lol
+      [n, m] = map read $ splitOn "x" lol
       rep = take n ys
   in concat (replicate m rep) ++ decompress (drop n ys)
 decompress (x:xs) = x : decompress xs
--- 2016/A9b.hs
+++ 2016/A9b.hs
@@ -1,29 +1,16 @@
-import Control.Monad
 import Data.List.Extra
 import Data.Maybe
-import qualified Data.Char as C
-import qualified Data.Map as Map
 
 ------
-iread :: String -> Int
-iread = read
-
+-- Routines I reuse for a lot of problems
 answer :: (Show a) => (String -> a) -> IO ()
 answer f = interact $ (++"\n") . show . f
-
-ord0 c = C.ord c - C.ord 'a'
-chr0 i = C.chr (i + C.ord 'a')
-incletter c i = chr0 ((ord0 c + i) `mod` 26)
-
 splitOn1 a b = fromJust $ stripInfix a b
-rsplitOn1 a b = fromJust $ stripInfixEnd a b
-
-
 --------
 
 decompress ('(':xs) =
   let (lol, ys) = splitOn1 ")" xs
-      [n, m] = map iread $ splitOn "x" lol
+      [n, m] = map read $ splitOn "x" lol
       rep = take n ys
   in (m * (decompress rep)) + decompress (drop n ys)
 decompress (x:xs) = 1 + (decompress xs)
