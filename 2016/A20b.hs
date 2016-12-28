4b5160be2cafc5fcd4f6e644eecf3d55808a92a1
Michael Sullivan <sully@msully.net>
2016-12-27 18:38:32 -0600

    Actually do day 20 part 2 right

--- 2016/A20b.hs
+++ 2016/A20b.hs
@@ -1,5 +1,10 @@
--- This is totally wrong but it somehow happens to work!
--- Apparently there aren't any gaps in my input of length > 1.
+-- OK so this version actually works.
+-- But I actually somewhat accidentally originally solved this problem by
+-- taking the length of the list that I produced in part 1...
+-- None of the gaps are longer than 1 element!
+
+-- This code could also produce the part 1 solution more efficient
+-- than part 1, though it doesn't seem to matter.
 
 import Control.Monad
 import Data.List.Extra
@@ -18,9 +23,15 @@ splitOn1 a b = fromJust $ stripInfix a b
 --------
 maxNum = 4294967295
 
-matches k (lo,hi) = (k >= lo && k <= hi) || k > maxNum
-allowed ls k = not $ any (matches k) ls
-solve rules = filter (allowed rules) candidates
-  where candidates = sort $ map ((+1) . snd) rules
+rmerge (lo,hi) ((lo',hi'):rest) =
+  if lo' <= hi+1 then rmerge (lo,max hi hi') rest
+  else (lo,hi) : rmerge (lo',hi') rest
+rmerge (lo,hi) [] = [(lo,hi)]
 
-main = answer $ length . solve . map ((read *** read) . splitOn1 "-") . lines
+canonify l = rmerge (-1,-1) $ sort $ (maxNum,maxNum):l
+
+gap (_,hi) (lo,_) = lo-hi-1
+
+solve l = sum $ zipWith gap l (tail l)
+
+main = answer $ solve . canonify . map ((read *** read) . splitOn1 "-") . lines
