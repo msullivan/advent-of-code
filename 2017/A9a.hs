425a8caacbb9fd2c9c56a3ea7c25212f31d349ea
Michael Sullivan <sully@msully.net>
2017-12-10 14:21:10 -0800

    day 9

---
+++ 2017/A9a.hs
@@ -0,0 +1,61 @@
+import Control.Monad
+import Data.List.Extra
+import Data.Maybe
+import qualified Data.Char as C
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+
+------
+iread :: String -> Int
+iread = read
+
+do2 f g x = (f x, g x)
+
+answer :: (Show a) => (String -> a) -> IO ()
+answer f = interact $ (++"\n") . show . f
+
+ord0 c = C.ord c - C.ord 'a'
+chr0 i = C.chr (i + C.ord 'a')
+incletter c i = chr0 ((ord0 c + i) `mod` 26)
+
+splitOn1 a b = fromJust $ stripInfix a b
+rsplitOn1 a b = fromJust $ stripInfixEnd a b
+
+-- pull out every part of a String that can be read in
+-- for some Read a and ignore the rest
+readOut :: Read a => String -> [a]
+readOut "" = []
+readOut s = case reads s of
+  [] -> readOut $ tail s
+  [(x, s')] -> x : readOut s'
+  _ -> error "ambiguous parse"
+ireadOut :: String -> [Int]
+ireadOut = readOut
+
+--------
+
+data Group = G [Group] | N
+  deriving Show
+
+stripGarbage ('>':l) = l
+stripGarbage ('!':_:l) = stripGarbage l
+stripGarbage (_:l) = stripGarbage l
+
+cleanGarbage ('<':l) = cleanGarbage (stripGarbage l)
+cleanGarbage (x:l) = x : cleanGarbage l
+cleanGarbage [] = []
+
+parseGroup ('{':r) =
+  let (gs, r') = parseGroups r
+  in (G gs, r')
+parseGroup r = (N, r)
+parseGroups ('}':r) = ([], r)
+parseGroups r =
+  case parseGroup r of
+    (g, ',':r') -> let (gs, r'') = parseGroups r' in (g:gs, r'')
+    (g, '}':r') -> ([g], r')
+
+score d N = 0
+score d (G gs) = d + sum (map (score (d+1)) gs)
+
+main = answer $ score 1 . fst . parseGroup . cleanGarbage . filter (/= '\n')
