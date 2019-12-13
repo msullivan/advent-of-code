a69e245aadbc98717e0e8323851b3fc4aeb9e8d4
Michael J. Sullivan <sully@msully.net>
2019-12-13 13:16:59 -0800

    make intcode a class in at least one test

--- 2019/11b.py
+++ 2019/11b.py
@@ -6,75 +6,90 @@ import time
 import itertools
 from collections import defaultdict
 
+class IntCode:
+    def __init__(self, program, input = None, output = None):
+        self.program = program.copy()
+        self.ip = 0
+        self.relative_base = 0
+        self.input = input or []
+        self.output = output or []
 
-def run(p, ip, input, output, relative_base):
-    if ip == -1:
-        return -1
+    @property
+    def done(self):
+        return self.ip == -1
 
-    while True:
-        instr = p[ip]
+    def run(self):
+        if self.done:
+            return
 
-        def read(i):
-            mode = (instr // (10**(1+i))) % 10
-            if mode == 0:
-                return p[p[ip+i]]
-            elif mode == 1:
-                return p[ip+i]
-            else:
-                return p[p[ip+i] + relative_base]
+        p = self.program
+        ip = self.ip
 
-        def write(i, v):
-            mode = (instr // (10**(1+i))) % 10
-            if mode == 0:
-                p[p[ip+i]] = v
-            else:
-                assert mode == 2
-                p[p[ip+i] + relative_base] = v
+        while True:
+            instr = p[ip]
 
+            def read(i):
+                mode = (instr // (10**(1+i))) % 10
+                if mode == 0:
+                    return p[p[ip+i]]
+                elif mode == 1:
+                    return p[ip+i]
+                else:
+                    return p[p[ip+i] + self.relative_base]
 
-        if instr % 100 == 1:
-            write(3, read(1) + read(2))
-            ip += 4
-        elif instr % 100 == 2:
-            write(3, read(1) * read(2))
-            ip += 4
-        elif instr % 100 == 3:
-            if not input:
-                return ip, relative_base
-            write(1, input.pop(0))
-            ip += 2
-        elif instr % 100 == 4:
-            output.append(read(1))
-            ip += 2
-        elif instr % 100 == 5:
-            if read(1) != 0:
-                ip = read(2)
-            else:
-                ip += 3
-        elif instr % 100 == 6:
-            if read(1) == 0:
-                ip = read(2)
-            else:
-                ip += 3
-        elif instr % 100 == 7:
-            if read(1) < read(2):
-                write(3, 1)
-            else:
-                write(3, 0)
-            ip += 4
-        elif instr % 100 == 8:
-            if read(1) == read(2):
-                write(3, 1)
-            else:
-                write(3, 0)
-            ip += 4
-        elif instr % 100 == 9:
-            relative_base += read(1)
-            ip += 2
-        elif instr % 100 == 99:
-            break
+            def write(i, v):
+                mode = (instr // (10**(1+i))) % 10
+                if mode == 0:
+                    p[p[ip+i]] = v
+                else:
+                    assert mode == 2
+                    p[p[ip+i] + self.relative_base] = v
 
-    return -1, 0
+            if instr % 100 == 1:
+                write(3, read(1) + read(2))
+                ip += 4
+            elif instr % 100 == 2:
+                write(3, read(1) * read(2))
+                ip += 4
+            elif instr % 100 == 3:
+                if not self.input:
+                    self.ip = ip
+                    return
+                write(1, self.input.pop(0))
+                ip += 2
+            elif instr % 100 == 4:
+                self.output.append(read(1))
+                ip += 2
+            elif instr % 100 == 5:
+                if read(1) != 0:
+                    ip = read(2)
+                else:
+                    ip += 3
+            elif instr % 100 == 6:
+                if read(1) == 0:
+                    ip = read(2)
+                else:
+                    ip += 3
+            elif instr % 100 == 7:
+                if read(1) < read(2):
+                    write(3, 1)
+                else:
+                    write(3, 0)
+                ip += 4
+            elif instr % 100 == 8:
+                if read(1) == read(2):
+                    write(3, 1)
+                else:
+                    write(3, 0)
+                ip += 4
+            elif instr % 100 == 9:
+                self.relative_base += read(1)
+                ip += 2
+            elif instr % 100 == 99:
+                break
+
+        self.ip = -1
+
 
 DIRS = [(0, -1), (1, 0), (0, 1), (-1, 0)]
 
@@ -86,21 +101,20 @@ def main(args):
     colors[0,0] = 1
     painted = set()
 
-    ip = 0
-    outs = []
+
+    interp = IntCode(p)
+
     pos = (0, 0)
     dir = 0
-    rb = 0
-    while ip != -1:
+    while not interp.done:
         ic = colors[pos]
-        ins = [colors[pos]]
-        print(ins)
+        interp.input = [colors[pos]]
 
-        ip, rb = run(p, ip, ins, outs, rb)
-        print(ip, ic, pos, outs, len(painted))
-        color, direction = outs
+        interp.run()
+        color, direction = interp.output
+        interp.output.clear()
 #        direction, color = color, direction
-        print(ip, color, direction)
+        print(color, direction)
         colors[pos] = color
         painted.add(pos)
         if direction == 0:
@@ -109,21 +123,6 @@ def main(args):
             dir = (dir + 1) % 4
         pos = (pos[0] + DIRS[dir][0], pos[1] + DIRS[dir][1])
 
-        outs.clear()
-
-        # minx = min(x for x, y in painted)
-        # miny = min(y for x, y in painted)
-        # maxx = max(x for x, y in painted)
-        # maxy = max(y for x, y in painted)
-        # for y in range(miny, maxy+1):
-        #     l = ""
-        #     for x in range(minx, maxx+1):
-        #         l += " #"[colors[x,y]]
-        #     print(l)
-        # print("========================")
-
-
-    print(outs)
     print(len(painted))
 
     minx = min(x for x, y in painted)
