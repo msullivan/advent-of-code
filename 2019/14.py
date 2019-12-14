e9813045f95b8d524e17c7938db0f4df7c29e7bf
Michael Sullivan <sully@msully.net>
2019-12-13 21:41:35 -0800

    day 14

---
+++ 2019/14.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+
+import sys
+
+def parse(s):
+    a, b = s.split(" ")
+    return int(a), b
+
+def has(side, x):
+    return any(x == s[1] for s in side)
+
+def consumes(rxn, x):
+    return has(rxn[0], x)
+
+def produces(rxn, x):
+    return has(rxn[1], x)
+
+
+def go(producers, num):
+    required = {"FUEL": num}
+    produced = {}
+    while True:
+        pending = {k for k, v in required.items() if v > produced.get(k, 0)}
+        if pending == {'ORE'}: break
+        to_try = next(x for x in pending if x != 'ORE')
+
+        rule = producers[to_try][0]
+        # print(rule)
+        num_produced = next(v for v, e in rule[1] if e == to_try)
+        # print(produced)
+        times = (required[to_try] - produced.get(to_try, 0) + (num_produced-1)) // num_produced
+        for v, e in rule[0]:
+            required[e] = required.get(e, 0) + times*v
+        for v, e in rule[1]:
+            produced[e] = produced.get(e, 0) + times*v
+
+        # print(required, produced)
+
+    return required['ORE']
+
+
+
+def main():
+    data = [s.strip() for s in sys.stdin]
+    rxns = []
+    for line in data:
+        l, r = line.split(" => ")
+        rxns.append(([parse(x) for x in l.split(", ")], [parse(x) for x in r.split(", ")]))
+
+    elements = {y for rxn in rxns for x, y in rxn[1]}
+
+    producers = {}
+    consumers = {}
+    for element in elements:
+        for rxn in rxns:
+            if consumes(rxn, element):
+                consumers.setdefault(element, []).append(rxn)
+            if produces(rxn, element):
+                producers.setdefault(element, []).append(rxn)
+
+    # These let me figure out that what producer to use was fixed
+    # print({k: len(v) for k, v in producers.items()})
+    # print({k: len(v) for k, v in consumers.items()})
+
+    part1 = go(producers, 1)
+
+    # binary search for it
+    n = 1
+    x = part1
+    while x < 1000000000000:
+        n *= 2
+        x = go(producers, n)
+        print(n, x)
+
+    lo = n // 2
+    hi = n
+    while lo < hi:
+        mid = (lo + hi) // 2
+        n = mid
+        x = go(producers, n)
+        print(n, x)
+        if x < 1000000000000:
+            lo = mid + 1
+        else:
+            hi = mid
+
+    part2 = lo - 1
+    print(part1)
+    print(part2)
+
+
+if __name__ == '__main__':
+    main()
