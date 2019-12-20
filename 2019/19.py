6a144f978b551eb028b8e88d7fe392044ea31536
Michael Sullivan <sully@msully.net>
2019-12-19 18:27:21 -0800

    day19 refactor

--- 2019/19.py
+++ 2019/19.py
@@ -7,97 +7,9 @@ import copy
 import sys
 import time
 from collections import defaultdict, deque
+from intcode import IntCode
 
-class IntCode:
-    def __init__(self, program, input = None, output = None):
-        self.program = program.copy()
-        self.ip = 0
-        self.relative_base = 0
-        self.input = input or []
-        self.output = output or []
-
-    @property
-    def done(self):
-        return self.ip == -1
-
-    def execute(self):
-        if self.done:
-            return
-
-        p = self.program
-        ip = self.ip
-
-        while True:
-            instr = p[ip]
-
-            def read(i):
-                mode = (instr // (10**(1+i))) % 10
-                if mode == 0:
-                    return p[p[ip+i]]
-                elif mode == 1:
-                    return p[ip+i]
-                else:
-                    return p[p[ip+i] + self.relative_base]
-
-            def write(i, v):
-                mode = (instr // (10**(1+i))) % 10
-                if mode == 0:
-                    p[p[ip+i]] = v
-                else:
-                    assert mode == 2
-                    p[p[ip+i] + self.relative_base] = v
-
-            if instr % 100 == 1:
-                write(3, read(1) + read(2))
-                ip += 4
-            elif instr % 100 == 2:
-                write(3, read(1) * read(2))
-                ip += 4
-            elif instr % 100 == 3:
-                if not self.input:
-                    self.ip = ip
-                    return
-                write(1, self.input.pop(0))
-                ip += 2
-            elif instr % 100 == 4:
-                self.output.append(read(1))
-                ip += 2
-            elif instr % 100 == 5:
-                if read(1) != 0:
-                    ip = read(2)
-                else:
-                    ip += 3
-            elif instr % 100 == 6:
-                if read(1) == 0:
-                    ip = read(2)
-                else:
-                    ip += 3
-            elif instr % 100 == 7:
-                if read(1) < read(2):
-                    write(3, 1)
-                else:
-                    write(3, 0)
-                ip += 4
-            elif instr % 100 == 8:
-                if read(1) == read(2):
-                    write(3, 1)
-                else:
-                    write(3, 0)
-                ip += 4
-            elif instr % 100 == 9:
-                self.relative_base += read(1)
-                ip += 2
-            elif instr % 100 == 99:
-                break
-
-        self.ip = -1
-
-    def run(self, input):
-        self.input = input
-        self.output = []
-        self.execute()
-        return self.output
-
+import array
 
 DIRS = [(0, -1), (1, 0), (0, 1), (-1, 0)]
 
@@ -120,13 +32,10 @@ def box(p, x, y, sz):
 
 def main(args):
     data = [s.strip() for s in sys.stdin]
-    lp = [int(x) for x in data[0].split(",")]
-    p = defaultdict(int, [(i, x) for i, x in enumerate(lp)])
+    p = array.array('q', [int(x) for x in data[0].split(",")])
     board = defaultdict(lambda: " ")
 
     # Part 1
-    interp = IntCode(p)
-    out = interp.run([])
 
     cnt = 0
     for x in range(50):
