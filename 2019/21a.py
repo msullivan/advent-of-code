4ab4bf4870b130fe3c48114a2d249b94de41bd97
Michael Sullivan <sully@msully.net>
2019-12-24 14:23:32 -0600

    day 21

---
+++ 2019/21a.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+# I solved this with programming but like not *correct* programming?
+# It works for the input I gave and was developed while looking at the map.
+
+import copy
+import sys
+import time
+from collections import defaultdict, deque
+from intcode import IntCode
+
+# !(b&c) =
+
+# (!b and d) | (!a)
+# ! (!(!b and d) & a)
+
+
+PROGRAM = """
+NOT B T
+NOT T T
+AND C T
+NOT T J
+AND D J
+NOT J J
+AND A J
+NOT J J
+WALK
+""".lstrip()
+
+
+def main(args):
+    data = [s.strip() for s in sys.stdin]
+    p = [int(x) for x in data[0].split(",")]
+    board = defaultdict(lambda: " ")
+
+    # Part 1
+    interp = IntCode(p)
+    out = interp.run([ord(c) for c in PROGRAM])
+
+    answer = None
+    if out[-1] > 255:
+        answer = out.pop()
+
+    s = "".join(chr(x) for x in out)
+    print(s)
+    m = s.split("\n")
+    print(answer)
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
