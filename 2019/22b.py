24b9ae695c2fe7e7f9d33263f2e8fa0be8314ac3
Michael J. Sullivan <sully@msully.net>
2020-01-06 14:30:15 -0800

    clean up 22b

--- 2019/22b.py
+++ 2019/22b.py
@@ -2,16 +2,11 @@
 
 from __future__ import print_function
 
-from collections import defaultdict, deque
-import sys
-import time
-import math
 import re
+import sys
 
 def extract(s):
     return [int(x) for x in re.findall(r'-?\d+', s)]
-
-DIRS = [(0, -1), (1, 0), (0, 1), (-1, 0)]
 
 N = 119315717514047
 REPS = 101741582076661
@@ -19,6 +14,7 @@ POS = 2020
 
 #N, REPS, POS  = 10, 2, 9
 
+# xgcd and modinv were copied from wikipedia or stack overflow or somewhere
 def xgcd(a, b):
     """return (g, x, y) such that a*x + b*y = g = gcd(a, b)"""
     x0, x1, y0, y1 = 0, 1, 1, 0
@@ -28,22 +24,27 @@ def xgcd(a, b):
         x0, x1 = x1, x0 - q * x1
     return b, x0, y0
 
+
 def modinv(a, b):
     """return x such that (x * a) % b == 1"""
+    # return pow(a, -1, b)  # In python3.8 you can just use pow!
     g, x, _ = xgcd(a, b)
-    # print(a, b, g, x%b)
-
-    if g == 1:
-        return x % b
-    assert False
+    assert g == 1
+    return x % b
 
 
-def compose(x1, x2):
-    return x1[0]*x2[0] % N, (x1[0]*x2[1] + x1[1]) % N
+def compose(f1, f2):
+    """Compose two shuffle functions.
+
+    f1(f2(x)) = a1*(a2*x + b2) + b1
+              = a1*a2*x + a1*b2 + b1
+    """
+    (a1, b1), (a2, b2) = f1, f2
+    return a1*a2 % N, (a1*b2 + b1) % N
 
 
 def exp(f, n):
-#    print(n)
+    """Do standard exponentiation by squaring"""
     if n == 0:
         return (1, 0)
     elif n % 2 == 0:
@@ -52,45 +53,31 @@ def exp(f, n):
         return compose(f, exp(compose(f, f), n // 2))
 
 
-
 def main(args):
     data = [s.strip() for s in sys.stdin]
     reps = int(args[1]) if args[1:] else REPS
+    pos = int(args[2]) if args[2:] else POS
 
-
-#    pos = 2020
-    pos = POS
-
-    ndata = []
+    # The transformation is equivalent to a function
+    # f(x) = (ax + b) % N, where f(x) is the position of
+    # card x after shuffling.
+    f = 1, 0
     for s in data:
         if s == "deal into new stack":
-            ndata.append("deal with %s" % (N-1))
-            ndata.append("cut 1")
+            op = -1, -1
+        elif s.startswith("deal with"):
+            op = extract(s)[0], 0
         else:
-            ndata.append(s)
+            op = 1, -extract(s)[0]
 
-    data = ndata
+        f = compose(op, f)
 
-    a, b = 1, 0
-    for s in data:
-        if s.startswith("deal with"):
-            (increment,) = extract(s)
-            #inv = modinv(increment, N)
-            mul = increment
-            a = (a * mul) % N
-            b = (b * mul) % N
-
-        else:
-            (cut,) = extract(s)
-            b = (b - cut) % N
-        print(s, (a,b))
-
-    af, bf = exp((a, b), reps)
-    print(af, bf)
-    #pos = (pos * af + bf) % N
+    af, bf = exp(f, reps)
+    # pos = ipos * af + bf  (mod N), so compute
+    # ipos = (pos - bf) * af^-1  (mod N)
     afn1 = modinv(af, N)
-    pos = ((pos - bf) * afn1) % N
-    print(pos)
+    ipos = ((pos - bf) * afn1) % N
+    print(ipos)
 
 if __name__ == '__main__':
     main(sys.argv)
