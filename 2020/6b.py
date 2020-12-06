5f53e3cfb277fdce95f454ed5d01c11600902282
Michael J. Sullivan <sully@msully.net>
2020-12-05 21:21:34 -0800

    day 6

---
+++ 2020/6b.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import sys
+import re
+
+def extract(s):
+    return [int(x) for x in re.findall(r'-?\d+', s)]
+
+
+def main(args):
+    data = [s.strip() for s in sys.stdin]
+    data.append('')
+
+    crap = []
+    cur = []
+    for x in data:
+        if not x:
+            crap.append(list(cur))
+            cur = []
+        else:
+            cur.extend(x.split(" "))
+
+    data = crap
+
+    total = 0
+    letters = "abcdefghijklmnopqrstuvwxyz"
+
+    for group in data:
+        seen = set(letters)
+        for person in group:
+            seen = seen & set(person)
+
+        total += len(seen)
+
+    print(total)
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
