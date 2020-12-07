bcdd54b87e6ed75e9ecf43cc57c0525bc4caa3ce
Michael J. Sullivan <sully@msully.net>
2020-12-06 21:25:30 -0800

    cleanup

--- 2020/7b.py
+++ 2020/7b.py
@@ -1,16 +1,10 @@
 #!/usr/bin/env python3
 
 import sys
-import re
-
-def extract(s):
-    return [int(x) for x in re.findall(r'-?\d+', s)]
-
 
 def search(bags, x):
     if x == 'shiny gold':
         return True
-    # print("A", x, bags[x][1])
     return any(search(bags, y[1])
                for y in bags[x])
 
@@ -21,12 +15,10 @@ def collect(bags, x):
 
 def main(args):
     data = [s.strip() for s in sys.stdin]
-    # data = [x.split('\n') for x in sys.stdin.read().split('\n\n')]
 
     bags = {}
     for line in data:
         bx,y = line.split(" bags contain ")
-        # print(x, y)
         y = y.replace("bags", "").replace("bag", "").replace(".", "")
         ys = y.split(", ")
         nys = []
@@ -35,22 +27,16 @@ def main(args):
             if x[0] == 'no': break
             nys.append((int(x[0]), " ".join(x[1:])))
 
-        # print((bx, nys))
         bags[bx] = nys
 
 
-    # print(bags)
     n = 0
     for k in bags:
         if search(bags, k) and k != 'shiny gold':
             n += 1
 
-    # print(len(bags))
     print(n)
-
     print(collect(bags, 'shiny gold')-1)
-
-    # print(data)
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv))
