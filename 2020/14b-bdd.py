d7152223058b21c49626ac24098bd44414a3b81c
Michael J. Sullivan <sully@msully.net>
2020-12-14 23:35:33 -0800

    Some cleanup

--- 2020/14b-bdd.py
+++ 2020/14b-bdd.py
@@ -5,20 +5,12 @@ from __future__ import annotations
 from typing import *
 
 from dataclasses import dataclass
-from collections import defaultdict
 from functools import lru_cache
 import sys
 import re
 
 def extract(s):
     return [int(x) for x in re.findall(r'-?\d+', s)]
-
-from itertools import chain, combinations
-def powerset(iterable):
-    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
-    s = list(iterable)
-    return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
-
 
 @dataclass(eq=False, frozen=True)
 class Node:
