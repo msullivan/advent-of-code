c79662ec3cabe2ddc11c9be1c5de354e2e31eeb0
Michael J. Sullivan <sully@msully.net>
2020-12-19 20:51:48 -0800

    clean up the matcher at the expense of the parser

--- 2020/19b.py
+++ 2020/19b.py
@@ -8,19 +8,17 @@ def extract(s):
     return [int(x) for x in re.findall(r'-?\d+', s)]
 
 def match(grammar, inp):
+    # I think this is actually a recursive version of the CYK
+    # algorithm.
     @functools.lru_cache(maxsize=None)
     def m(nr, t, s):
+        if nr == -1:
+            return t == s
         rule = grammar[nr]
         if isinstance(rule, str):
             return rule == inp[t:s]
 
-        for asdf in rule:
-            if len(asdf) == 1:
-                if m(asdf[0], t, s):
-                    return True
-                continue
-            p1, p2 = asdf
-
+        for p1, p2 in rule:
             for i in range(t, s):
                 if m(p1, t, i) and m(p2, i, s):
                     return True
@@ -46,10 +44,14 @@ def main(args):
             asdf = line.split(":")[1]
             res = []
             for part in asdf.split("|"):
-                res.append(extract(part))
+                xs = extract(part)
+                if len(xs) == 1:
+                    xs = [-1] + xs
+                assert len(xs) == 2
+                res.append(xs)
             grammar[k] = res
 
-    grammar[8] = [[42], [42, 8]]
+    grammar[8] = [[-1, 42], [42, 8]]
     # lol I can't handle 3-ary productions
     grammar[1000] = [[11, 31]]
     grammar[11] = [[42, 31], [42, 1000]]
