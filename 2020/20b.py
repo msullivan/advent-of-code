4b6916381778dbae3bae8bc048dda103af0e23bf
Michael J. Sullivan <sully@msully.net>
2020-12-19 23:02:26 -0800

    cleanup

--- 2020/20b.py
+++ 2020/20b.py
@@ -75,50 +75,31 @@ def main(args):
         n = extract(tile[0])[0]
         tiles[n] = tile[1:]
 
-    print(tiles)
     start, test = next(iter(tiles.items()))
-
-    print()
-    print('\n'.join(test))
-    print()
-    print('\n'.join(rotate(test)))
-
-    print(all_edges(test))
-    print(all_edges(rotate(test)))
 
     edge_map = defaultdict(list)
     for n, tile in tiles.items():
-        print("AAAA", tile)
         for e in all_edges(tile):
             edge_map[e].append(n)
-
-    print([len(x) for x in edge_map.values()])
-    print(edge_map)
 
     part1 = 1
     last = -1
     for n, tile in tiles.items():
         cnt = 0
         for e in edges(tile):
-            cnt += len(edge_map[e])
-        # print(cnt)
-        if cnt == 6:
-            print("HEY", n)
+            cnt += len(edge_map[e]) - 1
+        if cnt == 2:
             part1 *= n
             last = n
-    print(part1)
 
     ltile = tiles[last]
     while len(edge_map[ledge(ltile)]) == 2:
-        print('rotating initial')
         ltile = rotate(ltile)
     if len(edge_map[ltile[0]]) == 2:
-        print('flipping initial??')
         ltile = list(reversed(ltile))
 
     N = int(math.sqrt(len(tiles)))
 
-    print("starting in corner", n)
     mgrid = [[None] * N for x in range(N)]
     mgrid[0][0] = last, ltile
     y = 0
@@ -129,7 +110,6 @@ def main(args):
             me = [x for x in edge_map[be] if x != ln][0]
             mtile = tiles[me]
             while be != mtile[0]:
-                print('rotating')
                 mrev = list(reversed(mtile))
                 if be == mrev[0]:
                     mtile = mrev
@@ -146,7 +126,6 @@ def main(args):
             me = [x for x in edge_map[re] if x != ln][0]
             mtile = tiles[me]
             while re != ledge(mtile):
-                print('rotating')
                 mrev = list(reversed(mtile))
                 if re == ledge(mrev):
                     mtile = mrev
@@ -154,14 +133,10 @@ def main(args):
                 mtile = rotate(mtile)
 
             mgrid[y][x] = me, mtile
-            print(me, ln)
-            # break
 
-    # print(len(mgrid))
     ngrid = copy.deepcopy(mgrid)
     for i, row in enumerate(ngrid):
         for j, col in enumerate(ngrid[i]):
-            print(i, j)
             x = list(col[1])
             x.pop(0)
             x.pop()
@@ -169,26 +144,14 @@ def main(args):
                 x[k] = x[k][1:-1]
             ngrid[i][j] = x
 
-
-    # print("FUUUUUCK")
-    print(ngrid)
-
     pic = []
     for i, row in enumerate(ngrid):
-        for i2 in range(8):
+        for i2 in range(len(row[0])):
             s = ''
             for j, col in enumerate(ngrid[i]):
                 s += col[i2]
             pic.append(s)
 
-    print(pic)
-    print(len(pic))
-    print([len(x) for x in pic])
-
-    # pic = PIC
-    # pic = MONSTER + MONSTER
-    # print(pic)
-    print(MONSTER)
     cnt = 0
     for i in range(8):
         t = len(MONSTER[0])
@@ -206,15 +169,6 @@ def main(args):
                 if match:
                     cnt += 1
 
-
-                # print("CHECK")
-                # print(repr(pic[y][x:x+t]), repr(MONSTER[0]), pic[y][x:x+t] == MONSTER[0])
-                # print(repr(pic[y+1][x:x+t]), repr(MONSTER[1]), pic[y+1][x:x+t] == MONSTER[1])
-                # print(repr(pic[y+2][x:x+t]), repr(MONSTER[2]), pic[y+2][x:x+t] == MONSTER[2])
-                # print(repr(pic[y][x:x+t]), repr(MONSTER[0]))
-                # if pic[y][x:x+t] == MONSTER[0] and pic[y+1][x:x+t] == MONSTER[1] and pic[y+2][x:x+t] == MONSTER[2]:
-                #     cnt += 1
-        print("monster count!", cnt)
         if cnt:
             break
         pic = rotate(pic)
@@ -223,17 +177,8 @@ def main(args):
 
     everything = ''.join(pic).count('#')
     monster = ''.join(MONSTER).count('#')
-    print(everything, monster)
     print(part1)
     print(everything - monster*cnt)
-
-
-
-    # stile = test
-    # print(start, stile)
-    # for edge in edges(rotate(stile)):
-    #     print(edge)
-    #     print(edge_map[edge])
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv))
