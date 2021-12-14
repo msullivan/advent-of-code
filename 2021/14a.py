af06cf2b5eabd271cce18f219e317382fb08c539
Michael J. Sullivan <sully@msully.net>
2021-12-13 21:20:10 -0800

    day 14 cleanup

--- 2021/14a.py
+++ 2021/14a.py
@@ -2,40 +2,7 @@
 
 import sys
 
-from collections import defaultdict, Counter, deque
-from parse import parse
-import re
-
-def extract(s):
-    return [int(x) for x in re.findall(r'(-?\d+).?', s)]
-
-def vadd(v1, v2):
-    return tuple(x + y for x, y in zip(v1, v2))
-
-def ichr(i):
-    return chr(ord('a') + i)
-
-def iord(c):
-    return ord(c.lower()) - ord('a')
-
-def optidx(d, opt=max, nth=0):
-    if not isinstance(d, dict):
-        d = dict(enumerate(d))
-    rv = opt(d.values())
-    return [i for i, v in d.items() if v == rv][nth], rv
-
-LETTERS = "abcdefghijklmnopqrstuvwxyz"
-
-UP, RIGHT, DOWN, LEFT = VDIRS = (0, -1), (1, 0), (0, 1), (-1, 0),
-DIRS = {'N': UP, 'E': RIGHT, 'S': DOWN, 'W': LEFT }
-ALL_DIRS = [(x, y) for x in [-1,0,1] for y in [-1,0,1] if not x == y == 0]
-
-def turn(v, d='left'):
-    n = -1 if d == 'left' else 1
-    return VDIRS[(VDIRS.index(v) + n + len(VDIRS))%len(VDIRS)]
-
-
-##############################
+from collections import Counter
 
 def step(s, rules):
     out = ""
@@ -47,8 +14,6 @@ def step(s, rules):
     return out
 
 def main(args):
-    # data = [x.split('\n') for x in sys.stdin.read().split('\n\n')]
-    # data = [int(s.strip()) for s in sys.stdin]
     data = [s.strip() for s in sys.stdin]
     target = data[0]
     rules = [x.split(" -> ") for x in data[2:]]
@@ -57,7 +22,6 @@ def main(args):
     s = target
     for _ in range(10):
         s = step(s, rules)
-        # print(s)
 
     cnt = Counter(s)
     print(max(cnt.values()) - min(cnt.values()))
