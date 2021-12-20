c4b41eb790911c320fb3d43b483b3bd0d1e4a80c
Michael J. Sullivan <sully@msully.net>
2021-12-19 16:13:26 -0800

    make it fast and clean it up

--- 2021/19.py
+++ 2021/19.py
@@ -12,9 +12,6 @@ def vsub(v1, v2):
 def mdist(v1, v2):
     return sum(abs(x - y) for x, y in zip(v1, v2))
 
-def recenter(p, l):
-    return [vsub(x, p) for x in l]
-
 def crossp(a, b):
     a1, a2, a3 = a
     b1, b2, b3 = b
@@ -24,31 +21,35 @@ def dot(x, y):
     return sum(a*b for a, b in zip(x, y))
 
 def rotate(x, rot):
+    # ... this is a matrix multiplication
     i, j, k = rot
     return (dot(i, x), dot(j, x), dot(k, x))
+
+def rebase(p, rot, l):
+    return [vsub(rotate(x, rot), p) for x in l]
 
 
 def match(rots, s1, s2):
     ss1 = set(s1)
     for o1 in s1:
-        # print("OUT", o1)
-        rs1 = set(recenter(o1, s1))
-        for rot in rots:
-            rs2 = [rotate(x, rot) for x in s2]
-            for o2 in rs2:
-                # p = vsub(o2, o1)
-                cnt = 0
-                for z in rs2:
-                    if vsub(z, o2) in rs1:
-                        cnt += 1
-                if cnt >= 12:
-                    return o1, o2, rot, recenter(vsub(o2, o1), rs2)
+        mdists1 = set([mdist(o1, x) for x in s1])
+        for zo2 in s2:
+            # This is the big optimization that gets the time down to
+            # 3.75s (from like 2m): only try the rotations if there are
+            # at least 12 points in s2 with manhattan distances that
+            # also appear in 1.
+            mdists2 = [mdist(zo2, x) for x in s2]
+            if sum([x in mdists1 for x in mdists2]) < 12:
+                continue
 
-                # # print("IN", o1)
-                # rrs2 = recenter(vsub(o2, o1), rs2) # XXX
-                # matches = ss1 & set(rrs2)
-                # if len(matches) >= 12:
-                #     return (o1, o2, rot, rrs2)
+            # Alright actually try each rotation
+            for rot in rots:
+                delta = vsub(rotate(zo2, rot), o1)
+
+                rs2 = rebase(delta, rot, s2)
+                matches = ss1 & set(rs2)
+                if len(matches) >= 12:
+                    return delta, rot, rs2
 
     return None
 
@@ -58,14 +59,11 @@ def main(args):
     data = [[tuple(extract(s)) for s in x[1:] if s] for x in data]
 
     tops = [
-        (1,0),
-        (0,1),
-        (-1,0),
-        (0,-1),
+        (1,0), (0,1), (-1,0), (0,-1)
     ]
     rots = []
     for i in range(3):
-        for n in [-1, 1]:
+        for n in [1, -1]:
             for top in tops:
                 t = list(top)
                 t.insert(i, 0)
@@ -73,16 +71,8 @@ def main(args):
                 f.insert(i, n)
                 rots.append((tuple(f), tuple(t), crossp(f, t)))
 
-    # XXX: is this really needed
-    for i, (x, y, z) in enumerate(rots):
-        if rotate((1,2,3), (x,y,z)) == (1,2,3):
-            id_rot = i
-            break
-    else:
-        raise AssertionError
-
     solved = {}
-    solved[0] = ((0, 0, 0), rots[id_rot])
+    solved[0] = ((0, 0, 0), rots[0])
     done = set()
     wl = [0]
 
@@ -93,20 +83,18 @@ def main(args):
         d1 = data[i]
 
         pos, rot = solved[i]
-        d1 = [rotate(x, rot) for x in d1]
-        d1 = recenter(pos, d1) # XXX
+        d1 = rebase(pos, rot, d1)
 
         for j, d2 in enumerate(data):
             if j in done or i == j: continue
 
             res = match(rots, d1, d2)
             if res:
-                a1, b1, r, new = res
+                delta, nrot, new = res
                 sensors.update(new)
-                delta = vsub(b1, a1)
-                print("REL", delta, r, i, j)
+                print("FOUND", vsub((0,0,0), delta), nrot, i, j)
                 wl.append(j)
-                solved[j] = (delta, r)
+                solved[j] = (delta, nrot)
         done.add(i)
 
     print(len(sensors))
