42ff32aa3b8cb762611a0ff368569868f1b1653f
Michael J. Sullivan <sully@msully.net>
2021-12-20 23:30:38 -0800

    cleanup

--- 2021/21b.py
+++ 2021/21b.py
@@ -7,85 +7,55 @@ import re
 def extract(s):
     return [int(x) for x in re.findall(r'(-?\d+).?', s)]
 
+def upd(t, i, x):
+    t = list(t)
+    t[i] = x
+    return tuple(t)
+
+
+def go(player, states, done):
+    # advance all the pieces three times
+    for _ in range(3):
+        nstates = defaultdict(int)
+        for move in range(1, 4):
+            for state, cnt in states.items():
+                p, score = state[player]
+                np = p + move
+                while np > 10:
+                    np -= 10
+                nstates[upd(state, player, (np, score))] += cnt
+        states = nstates
+
+    # score
+    nstates = defaultdict(int)
+    for state, cnt in states.items():
+        p, score = state[player]
+        nstates[upd(state, player, (p, p+score))] += cnt
+    states = nstates
+
+    dead = [
+        (((b, p1), (a, p2)), n)
+        for ((b, p1), (a, p2)), n in states.items()
+        if (n == 0 or p1 >= 21 or p2 >= 21)
+    ]
+    done.update(dead)
+    for x, _ in dead:
+        del states[x]
+
+    return states
+
 
 def main(args):
     data = [s.strip() for s in sys.stdin]
     p1 = extract(data[0])[1]
     p2 = extract(data[1])[1]
 
-    die = 1
-    nrolls = 0
-
-    def roll():
-        nonlocal die, nrolls
-        nrolls += 1
-        x = die
-        die = (die + 1)
-        if die == 101:
-            die = 1
-        return x
-
     done = set()
-
-    s1 = s2 = 0
     states = defaultdict(int, {((p1, 0), (p2, 0)): 1})
-    while True:
-        # print(states)
-        for _ in range(3):
-            nstates = defaultdict(int)
-            for move in range(3):
-                for ((p, score), z), cnt in states.items():
-                    np = p + move + 1
-                    while np > 10:
-                        np -= 10
-                    nstates[(np, score), z] += cnt
-            states = nstates
-        # score
-        nstates = defaultdict(int)
-        for ((p, score), z), cnt in states.items():
-            nstates[(p, p+score), z] += cnt
-        states = nstates
 
-        # WIN CHECK
-        dead = [
-            (((b, p1), (a, p2)), n)
-            for ((b, p1), (a, p2)), n in states.items()
-            if (n == 0 or p1 >= 21 or p2 >= 21)
-        ]
-        done.update(dead)
-        for x, _ in dead:
-            del states[x]
-
-        if not states:
-            break
-
-        # COPY PASTE
-        for _ in range(3):
-            nstates = defaultdict(int)
-            for move in range(3):
-                for (z, (p, score)), cnt in states.items():
-                    np = p + move + 1
-                    while np > 10:
-                        np -= 10
-                    nstates[z, (np, score)] += cnt
-            states = nstates
-        # score
-        nstates = defaultdict(int)
-        for (z, (p, score)), cnt in states.items():
-            nstates[z, (p, p+score)] += cnt
-        states = nstates
-
-        dead = [
-            (((b, p1), (a, p2)), n)
-            for ((b, p1), (a, p2)), n in states.items()
-            if (n == 0 or p1 >= 21 or p2 >= 21)
-        ]
-        done.update(dead)
-        for x, _ in dead:
-            del states[x]
-
-        if not states:
-            break
+    while states:
+        for player in range(2):
+            states = go(player, states, done)
 
     n1 = sum(n for ((_, p1), (_, p2)), n in done if p1 >= 21)
     n2 = sum(n for ((_, p1), (_, p2)), n in done if p2 >= 21)
