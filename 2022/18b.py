839749f8f487d9135d5b3216f4b2d95eacfa1825
Michael J. Sullivan <sully@msully.net>
2022-12-17 21:34:44 -0800

    Make it sound

--- 2022/18b.py
+++ 2022/18b.py
@@ -9,9 +9,6 @@ def extract(s):
 def vadd(v1, v2):
     return tuple(x + y for x, y in zip(v1, v2))
 
-def mag(v):
-    return abs(v[0]) + abs(v[1]) + abs(v[2])
-
 VDIRS3 = (0, -1, 0), (1, 0, 0), (0, 1, 0), (-1, 0, 0), (0, 0, 1), (0, 0, -1)
 
 def main(args):
@@ -28,25 +25,32 @@ def main(args):
     print(total)
 
     # Part 2
+    # Find a bounding box for all the points
+    coords = list(zip(*pts))
+    pmin = tuple(min(c)-1 for c in coords)
+    pmax = tuple(max(c)+1 for c in coords)
+    print(pmin, pmax)
 
-    # Find all of the squares reachable from "outside".
-    # This is unsound, since I just eyeballed the input, saw it looked like
-    # everything was greater than 0, and figured we were good.
-
-    wl = [(0,0,0)]
-    avail = set()
+    # Find all of the squares that are "outside".
+    # My original solution that finished #16th did an unsound version of this,
+    # based on eyeballing the input: I always started at (0, 0, 0) and bounded
+    # it at points with magnitude 50.
+    wl = [(pmin)]
+    outside = set()
     while wl:
         x = wl.pop()
-        avail.add(x)
+        outside.add(x)
         for d in VDIRS3:
             nb = vadd(x, d)
-            if nb not in pts and mag(nb) < 50 and nb not in avail:
+            if nb not in pts and nb not in outside and all(
+                pmin[i] <= nb[i] <= pmax[i] for i in (0, 1, 2)
+            ):
                 wl.append(nb)
 
     total = 0
     for pt in pts:
         for d in VDIRS3:
-            if vadd(pt, d) in avail:
+            if vadd(pt, d) in outside:
                 total += 1
 
     print(total)
