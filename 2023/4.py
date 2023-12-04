ab4d3c30f9d8447f6b3d7417c6fe167f0fd51c1f
Michael J. Sullivan <sully@msully.net>
2023-12-03 21:23:10 -0800

    day 4

---
+++ 2023/4.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+
+import sys
+from collections import defaultdict, Counter, deque
+from parse import parse
+import re
+import math
+
+def extract(s):
+    return [int(x) for x in re.findall(r'(-?\d+).?', s)]
+
+def vadd(v1, v2):
+    return tuple([x + y for x, y in zip(v1, v2)])
+
+def ichr(i):
+    return chr(ord('a') + i)
+
+def iord(c):
+    return ord(c.lower()) - ord('a')
+
+def optidx(d, opt=max, nth=0):
+    if not isinstance(d, dict):
+        d = dict(enumerate(d))
+    rv = opt(d.values())
+    return [i for i, v in d.items() if v == rv][nth], rv
+
+LETTERS = "abcdefghijklmnopqrstuvwxyz"
+
+UP, RIGHT, DOWN, LEFT = VDIRS = (0, -1), (1, 0), (0, 1), (-1, 0),
+DIRS = {'N': UP, 'E': RIGHT, 'S': DOWN, 'W': LEFT }
+ALL_DIRS = [(x, y) for x in [-1,0,1] for y in [-1,0,1] if not x == y == 0]
+
+def turn(v, d='left'):
+    n = -1 if d == 'left' else 1
+    return VDIRS[(VDIRS.index(v) + n + len(VDIRS))%len(VDIRS)]
+
+
+##############################
+
+def main(args):
+    file = open(args[1]) if len(args) > 1 else sys.stdin
+    # data = [x.rstrip('\n').split('\n') for x in file.read().split('\n\n')]
+    # data = [int(s.rstrip('\n')) for s in file]
+    data = [s.rstrip('\n') for s in file]
+
+    copies = [1 for _ in data]
+
+    tscore = 0
+    tot = 0
+    for i, line in enumerate(data):
+        n, r = line.split(':')
+        win, have = r.split('|')
+
+        win = extract(win)
+        have = extract(have)
+
+        score = cnt = 0
+        for n in have:
+            if n in win:
+                cnt += 1
+
+                if score == 0:
+                    score = 1
+                else:
+                    score *= 2
+
+
+        for j in range(i + 1, i + 1 + cnt):
+            copies[j] += copies[i]
+
+        tscore += score
+        tot += copies[i]
+
+    print(tscore)
+    print(tot)
+
+
+if __name__ == '__main__':
+    main(sys.argv)
