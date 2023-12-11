3ea2cf2556fefeb7deadb74408ed8207bdff3034
Michael J. Sullivan <sully@msully.net>
2023-12-10 21:24:10 -0800

    clean up 11a

--- 2023/11a.py
+++ 2023/11a.py
@@ -1,56 +1,17 @@
 #!/usr/bin/env python3
 
 import sys
-from collections import defaultdict, Counter, deque
-from parse import parse
-import re
-import math
-
-def extract(s):
-    return [int(x) for x in re.findall(r'(-?\d+).?', s)]
-
-def vadd(v1, v2):
-    return tuple([x + y for x, y in zip(v1, v2)])
-
-def ichr(i):
-    return chr(ord('a') + i)
-
-def iord(c):
-    return ord(c.lower()) - ord('a')
-
-def optidx(d, opt=max, nth=0):
-    if not isinstance(d, dict):
-        d = dict(enumerate(d))
-    rv = opt(d.values())
-    return [i for i, v in d.items() if v == rv][nth], rv
-
-LETTERS = "abcdefghijklmnopqrstuvwxyz"
-
-UP, RIGHT, DOWN, LEFT = VDIRS = (0, -1), (1, 0), (0, 1), (-1, 0),
-DIRS = {'N': UP, 'E': RIGHT, 'S': DOWN, 'W': LEFT }
-ALL_DIRS = [(x, y) for x in [-1,0,1] for y in [-1,0,1] if not x == y == 0]
-
-def turn(v, d='left'):
-    n = -1 if d == 'left' else 1
-    return VDIRS[(VDIRS.index(v) + n + len(VDIRS))%len(VDIRS)]
-
-
-##############################
 
 def main(args):
     file = open(args[1]) if len(args) > 1 else sys.stdin
-    # data = [x.rstrip('\n').split('\n') for x in file.read().split('\n\n')]
-    # data = [int(s.rstrip('\n')) for s in file]
     data = [s.rstrip('\n') for s in file]
 
     cols = set()
     rows = set()
     galaxies = set()
 
-    m = defaultdict(lambda: '.')
     for y, l in enumerate(data):
         for x, c in enumerate(l):
-            m[x, y] = c
             if c == '#':
                 galaxies.add((x, y))
                 rows.add(y)
@@ -74,10 +35,8 @@ def main(args):
 
     galaxies = []
 
-    m = defaultdict(lambda: '.')
     for y, l in enumerate(new2):
         for x, c in enumerate(l):
-            m[x, y] = c
             if c == '#':
                 galaxies.append((x, y))
                 rows.add(y)
