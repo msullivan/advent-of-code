cd04da456fe30be5336b773bc7d1e8af4f413d4c
Michael J. Sullivan <sully@msully.net>
2023-12-31 22:19:29 -0800

    slightly optimize more

--- 2023/25.py
+++ 2023/25.py
@@ -24,23 +24,18 @@ def contract(graph, edges):
     N = len(graph)
     M = len(edges)
     while N > 2:
-        # print(M)
-        # print(len(edges), M)
-        assert len(edges) == M
-        i = random.randrange(0, len(edges))
+        i = random.randrange(0, M)
         e = edges[i]
-        edges[i], edges[-1] = edges[-1], edges[i]
-        del edges[-1]
+        edges[i], edges[M-1] = edges[M-1], edges[i]
         M -= 1
 
         e0 = lookup(merges, e[0])
         e1 = lookup(merges, e[1])
 
-        if e1 == e0:
+        if e0 == e1:
             continue
 
         N -= 1
-
         merges[e0] = e1
 
     groups = {}
@@ -56,8 +51,6 @@ def contract(graph, edges):
 
 def main(args):
     file = open(args[1]) if len(args) > 1 else sys.stdin
-    # data = [x.rstrip('\n').split('\n') for x in file.read().split('\n\n')]
-    # data = [int(s.rstrip('\n')) for s in file]
     data = [s.rstrip('\n') for s in file]
 
     random.seed(1338)
@@ -72,15 +65,18 @@ def main(args):
             graph[k].append(s)
             edges.append(tuple(sorted([k, s])))
 
-    print(graph)
+    first = None
     # for i in itertools.count(1):
-    for i in range(200):
+    for i in range(400):
         cuts, n1s, n2s = contract(graph, edges)
         print(i, cuts)
         if len(cuts) == 3:
+            if first is None:
+                first = i
             res = len(n1s)*len(n2s)
             # break
 
+    print(first)
     print(res)
 
 if __name__ == '__main__':
