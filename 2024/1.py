999fb56258ead4ddbc88265e8a7254447ba6e677
Michael J. Sullivan <sully@msully.net>
2024-11-30 21:11:12 -0800

    Day 1

---
+++ 2024/1.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import sys
+import re
+from collections import Counter
+
+def extract(s):
+    return [int(x) for x in re.findall(r'(-?\d+).?', s)]
+
+
+def main(args):
+    file = open(args[1]) if len(args) > 1 else sys.stdin
+    data = [extract(s.rstrip('\n')) for s in file]
+
+    # ... I couldn't remember the unzip trick
+    xs, ys = [], []
+    for x, y in data:
+        xs.append(x)
+        ys.append(y)
+
+    xs.sort()
+    ys.sort()
+
+    asdf = Counter(ys)
+
+    print(sum(abs(x-y) for x, y in zip(xs, ys)))
+    print(sum(x * asdf[x] for x in xs))
+
+
+if __name__ == '__main__':
+    main(sys.argv)
