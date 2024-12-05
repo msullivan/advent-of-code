c51e86128bc57d7473faf2ef369e99549e3e4f4b
Michael J. Sullivan <sully@msully.net>
2024-12-04 21:34:08 -0800

    cleanup

--- 2024/5.py
+++ 2024/5.py
@@ -1,51 +1,16 @@
 #!/usr/bin/env python3
 
 import sys
-from collections import defaultdict, Counter, deque
-from parse import parse
 import re
-import math
 import functools
 
 def extract(s):
     return [int(x) for x in re.findall(r'(-?\d+).?', s)]
 
-def vadd(v1, v2):
-    return tuple([x + y for x, y in zip(v1, v2)])
-
-def ichr(i):
-    return chr(ord('a') + i)
-
-def iord(c):
-    return ord(c.lower()) - ord('a')
-
-def optidx(d, opt=max, nth=0):
-    if not isinstance(d, dict):
-        d = dict(enumerate(d))
-    rv = opt(d.values())
-    return [i for i, v in d.items() if v == rv][nth], rv
-
-LETTERS = "abcdefghijklmnopqrstuvwxyz"
-
-UP, RIGHT, DOWN, LEFT = VDIRS = (0, -1), (1, 0), (0, 1), (-1, 0),
-DIRS = {'N': UP, 'E': RIGHT, 'S': DOWN, 'W': LEFT }
-ALL_DIRS = [(x, y) for x in [-1,0,1] for y in [-1,0,1] if not x == y == 0]
-
-def turn(v, d='left'):
-    n = -1 if d == 'left' else 1
-    return VDIRS[(VDIRS.index(v) + n) % len(VDIRS)]
-
-
-##############################
 
 def main(args):
     file = open(args[1]) if len(args) > 1 else sys.stdin
     data = [x.rstrip('\n').split('\n') for x in file.read().split('\n\n')]
-    # data = [int(s.rstrip('\n')) for s in file]
-    # data = [s.rstrip('\n') for s in file]
-
-    print(data[0])
-    print(data[1])
 
     deps = {}
     for x in data[0]:
@@ -65,16 +30,13 @@ def main(args):
     for line in data[1]:
         nums = extract(line)
         seen = set()
-        print(nums)
         ok = True
         for num in nums:
             for other in deps.get(num, []):
                 if other in nums and other not in seen:
-                    print('!', other)
                     ok = False
             seen.add(num)
         if ok:
-            print('OK', nums, nums[len(nums)//2 + 1])
             s += nums[len(nums)//2]
 
         if not ok:
