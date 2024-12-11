2b2b82f91ad2df5c52c2a4b8a12b91df2fe6fafa
Michael J. Sullivan <sully@msully.net>
2024-12-10 21:14:34 -0800

    day 11

---
+++ 2024/11a.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+import sys
+from collections import defaultdict, Counter, deque
+from parse import parse
+import re
+import math
+import itertools
+
+def extract(s):
+    return [int(x) for x in re.findall(r'(-?\d+).?', s)]
+
+def vadd(v1, v2):
+    if len(v1) == 2:
+        return v1[0] + v2[0], v1[1] + v2[1]
+    elif len(v1) == 2:
+        return v1[0] + v2[0], v1[1] + v2[1], v1[2] + v2[2]
+    else:
+        # ... this is so slow.
+        return tuple([x + y for x, y in zip(v1, v2)])
+
+def ichr(i):
+    return chr(ord('a') + i)
+
+def iord(c):
+    return ord(c.lower()) - ord('a')
+
+def optidx(d, opt=max, nth=0):
+    if not isinstance(d, dict):
+        d = dict(enumerate(d))
+    rv = opt(d.values())
+    return [i for i, v in d.items() if v == rv][nth], rv
+
+LETTERS = "abcdefghijklmnopqrstuvwxyz"
+
+UP, RIGHT, DOWN, LEFT = VDIRS = (0, -1), (1, 0), (0, 1), (-1, 0),
+DIRS = {'N': UP, 'E': RIGHT, 'S': DOWN, 'W': LEFT }
+ALL_DIRS = [(x, y) for x in [-1,0,1] for y in [-1,0,1] if not x == y == 0]
+
+def turn(v, d='left'):
+    n = -1 if d == 'left' else 1
+    return VDIRS[(VDIRS.index(v) + n) % len(VDIRS)]
+
+
+##############################
+
+def main(args):
+    file = open(args[1]) if len(args) > 1 else sys.stdin
+    # data = [x.rstrip('\n').split('\n') for x in file.read().split('\n\n')]
+    # data = [int(s.rstrip('\n')) for s in file]
+    data = [s.rstrip('\n') for s in file]
+
+    l = extract(data[0])
+
+    for i in range(25):
+        print(len(l))
+        nl = []
+        for x in l:
+            sx = str(x)
+            if x == 0:
+                nl.append(1)
+            elif len(sx) % 2 == 0:
+                nl.append(int(sx[:len(sx)//2]))
+                nl.append(int(sx[len(sx)//2:]))
+            else:
+                nl.append(x*2024)
+
+        l = nl
+
+    print(len(l))
+
+if __name__ == '__main__':
+    main(sys.argv)
---
+++ 2024/11b.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+
+import sys
+from collections import defaultdict, Counter, deque
+from parse import parse
+import re
+import math
+import itertools
+
+def extract(s):
+    return [int(x) for x in re.findall(r'(-?\d+).?', s)]
+
+def vadd(v1, v2):
+    if len(v1) == 2:
+        return v1[0] + v2[0], v1[1] + v2[1]
+    elif len(v1) == 2:
+        return v1[0] + v2[0], v1[1] + v2[1], v1[2] + v2[2]
+    else:
+        # ... this is so slow.
+        return tuple([x + y for x, y in zip(v1, v2)])
+
+def ichr(i):
+    return chr(ord('a') + i)
+
+def iord(c):
+    return ord(c.lower()) - ord('a')
+
+def optidx(d, opt=max, nth=0):
+    if not isinstance(d, dict):
+        d = dict(enumerate(d))
+    rv = opt(d.values())
+    return [i for i, v in d.items() if v == rv][nth], rv
+
+LETTERS = "abcdefghijklmnopqrstuvwxyz"
+
+UP, RIGHT, DOWN, LEFT = VDIRS = (0, -1), (1, 0), (0, 1), (-1, 0),
+DIRS = {'N': UP, 'E': RIGHT, 'S': DOWN, 'W': LEFT }
+ALL_DIRS = [(x, y) for x in [-1,0,1] for y in [-1,0,1] if not x == y == 0]
+
+def turn(v, d='left'):
+    n = -1 if d == 'left' else 1
+    return VDIRS[(VDIRS.index(v) + n) % len(VDIRS)]
+
+
+##############################
+
+def main(args):
+    file = open(args[1]) if len(args) > 1 else sys.stdin
+    # data = [x.rstrip('\n').split('\n') for x in file.read().split('\n\n')]
+    # data = [int(s.rstrip('\n')) for s in file]
+    data = [s.rstrip('\n') for s in file]
+
+    l = extract(data[0])
+
+    m = defaultdict(lambda: 0)
+    for x in l:
+        m[x] = 1
+
+    for i in range(75):
+        nm = defaultdict(lambda: 0)
+        for x, cnt in m.items():
+            sx = str(x)
+            if x == 0:
+                nm[1] += cnt
+            elif len(sx) % 2 == 0:
+                nm[int(sx[:len(sx)//2])] += cnt
+                nm[int(sx[len(sx)//2:])] += cnt
+            else:
+                nm[x*2024] += cnt
+
+        m = nm
+
+    print(sum(nm.values()))
+
+if __name__ == '__main__':
+    main(sys.argv)
