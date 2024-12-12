c56ebd2bcd76d50ebbbac7d535edd9e817fc291e
Michael J. Sullivan <sully@msully.net>
2024-12-11 21:46:36 -0800

    fix vadd

--- 2024/12.py
+++ 2024/12.py
@@ -7,14 +7,7 @@ def extract(s):
     return [int(x) for x in re.findall(r'(-?\d+).?', s)]
 
 def vadd(v1, v2):
-    if len(v1) == 2:
-        return v1[0] + v2[0], v1[1] + v2[1]
-    elif len(v1) == 2:
-        return v1[0] + v2[0], v1[1] + v2[1], v1[2] + v2[2]
-    else:
-        # ... this is so slow.
-        return tuple([x + y for x, y in zip(v1, v2)])
-
+    return v1[0] + v2[0], v1[1] + v2[1]
 
 UP, RIGHT, DOWN, LEFT = VDIRS = (0, -1), (1, 0), (0, 1), (-1, 0),
 
--- template.py
+++ template.py
@@ -13,7 +13,7 @@ def extract(s):
 def vadd(v1, v2):
     if len(v1) == 2:
         return v1[0] + v2[0], v1[1] + v2[1]
-    elif len(v1) == 2:
+    elif len(v1) == 3:
         return v1[0] + v2[0], v1[1] + v2[1], v1[2] + v2[2]
     else:
         # ... this is so slow.
