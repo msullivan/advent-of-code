918854ff14fdae77f5e17b9157aed8fa276379ef
Michael J. Sullivan <sully@msully.net>
2024-12-17 22:36:54 -0800

    Day 18

---
+++ 2024/18.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+
+import sys
+from collections import defaultdict, Counter, deque
+import re
+import heapq
+
+def extract(s):
+    return [int(x) for x in re.findall(r'(-?\d+).?', s)]
+
+def vadd(v1, v2):
+    if len(v1) == 2:
+        return v1[0] + v2[0], v1[1] + v2[1]
+    elif len(v1) == 3:
+        return v1[0] + v2[0], v1[1] + v2[1], v1[2] + v2[2]
+    else:
+        # ... this is so slow.
+        return tuple([x + y for x, y in zip(v1, v2)])
+
+UP, RIGHT, DOWN, LEFT = VDIRS = (0, -1), (1, 0), (0, 1), (-1, 0),
+
+def gnbrs(s, dirs=VDIRS):
+    return [(dir, vadd(s, dir)) for dir in dirs]
+
+# This is A* if you pass a heuristic and a target, otherwise Dijkstra's
+# edges should return a sequence of (nbr, weight) pairs
+def dijkstra(m, edges, start, heuristic=None, target=None):
+    cost = {start: 0}
+    path = {}
+    todo = [(0, 0, start)]
+    explored = 0
+
+    while todo and todo[0][-1] != target:
+        _, k, cur = heapq.heappop(todo)
+        if k != cost[cur]:
+            continue
+        explored += 1
+
+        nbrs = list(edges(m, cur))
+        for nbr, weight in nbrs:
+            ncost = cost[cur] + weight
+            if nbr not in cost or ncost < cost[nbr]:
+                cost[nbr] = ncost
+                path[nbr] = cur
+                hcost = ncost if not heuristic else ncost + heuristic(nbr)
+                heapq.heappush(todo, (hcost, ncost, nbr))
+
+    return cost, path
+
+
+def main(args):
+    file = open(args[1]) if len(args) > 1 else sys.stdin
+    # data = [x.rstrip('\n').split('\n') for x in file.read().split('\n\n')]
+    # data = [int(s.rstrip('\n')) for s in file]
+    data = [s.rstrip('\n') for s in file]
+
+    m = defaultdict(lambda: '.')
+    for entry in data[:1024]:
+        x, y = extract(entry)
+        m[x,y] = '#'
+
+    M = 70
+
+    def nbrs(m, p):
+        for _, n in gnbrs(p):
+            if 0 <= n[0] <= M and 0 <= n[1] <= M and m[n] == '.':
+                yield n, 1
+
+    start = 0, 0
+
+    ds, _ = dijkstra(m, nbrs, start)
+    p1 = ds[M, M]
+
+    for i in range(1024, len(data)):
+        # print(i)
+        x, y = extract(data[i])
+        m[x,y] = '#'
+        ds, _ = dijkstra(m, nbrs, start)
+        if (M, M) not in ds:
+            break
+
+    print(p1)
+    print(f'{x},{y}')
+
+
+if __name__ == '__main__':
+    main(sys.argv)
