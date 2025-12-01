6ab5b029ba56737e4047a017e313718a5ff8fdf6
Michael J. Sullivan <sully@msully.net>
2025-11-30 21:32:04 -0800

    part 1, maths

--- 2025/1b.py
+++ 2025/1b.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
-from collections import defaultdict, Counter, deque
-# from parse import parse
 import re
-import math
-import itertools
-import heapq
 
 def extract(s):
     return [int(x) for x in re.findall(r'(-?\d+).?', s)]
@@ -14,22 +9,19 @@ def extract(s):
 
 def main(args):
     file = open(args[1]) if len(args) > 1 else sys.stdin
-    # data = [x.rstrip('\n').split('\n') for x in file.read().split('\n\n')]
-    # data = [int(s.rstrip('\n')) for s in file]
     data = [s.rstrip('\n') for s in file]
 
     pos = 50
     cnt = 0
     for line in data:
         m = -1 if line[0] == 'L' else 1
-        n = extract(line)[0]
-        for _ in range(n):
-            n = 1
-            opos = pos
-            pos = (pos + m * n) % 100
-            cnt += (pos == 0)
-        # cnt += (n // 100)
+        n, = extract(line)
 
+        # maths!  (original solution looped)
+        npos = (pos + m * n)
+        cnt += pos != 0 and npos <= 0
+        cnt += abs(npos) // 100
+        pos = npos % 100
 
     print(cnt)
 
