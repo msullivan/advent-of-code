577b063f842ffceca058bb1dcf4f0c7a0b09fd80
Michael J. Sullivan <sully@msully.net>
2025-12-01 21:15:55 -0800

    day 2

---
+++ 2025/A2b.hs
@@ -0,0 +1,51 @@
+import Control.Monad
+import Data.List.Extra
+import Data.Maybe
+import qualified Data.Char as C
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+import Debug.Trace
+
+------
+iread :: String -> Int
+iread = read
+
+do2 f g x = (f x, g x)
+
+answer :: (Show a) => (String -> a) -> IO ()
+answer f = interact $ (++"\n") . show . f
+
+ord0 c = C.ord c - C.ord 'a'
+chr0 i = C.chr (i + C.ord 'a')
+incletter c i = chr0 ((ord0 c + i) `mod` 26)
+
+splitOn1 a b = fromJust $ stripInfix a b
+rsplitOn1 a b = fromJust $ stripInfixEnd a b
+
+-- pull out every part of a String that can be read in
+-- for some Read a and ignore the rest
+readOut :: Read a => String -> [a]
+readOut "" = []
+readOut s = case reads s of
+  [] -> readOut $ tail s
+  [(x, s')] -> x : readOut s'
+  _ -> error "ambiguous parse"
+ireadOut :: String -> [Int]
+ireadOut = readOut
+
+traceShow' s = traceShow s s
+
+--------
+
+isBad n =
+  let sn = show n
+      ln = length sn
+      check i = sn == (concat $ replicate (ln `div` i) (take i sn))
+  in any check [1..ln-1]
+
+parse = map (\[x, y] -> (x, -y)) . map ireadOut . wordsBy (==',') . head . lines
+
+solve lines = sum (map check lines)
+  where check (lo, hi) = sum $ filter isBad [lo..hi]
+
+main = answer $ solve . parse
