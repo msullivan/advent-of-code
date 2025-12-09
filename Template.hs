57fa34954ab84861bfc1d4e6f18b0402256ffc5c
Michael J. Sullivan <sully@msully.net>
2025-12-09 17:15:13 +1300

    haskell template

---
+++ Template.hs
@@ -0,0 +1,46 @@
+import Control.Monad
+import Data.List
+import Data.List.Split
+import Data.Maybe
+import qualified Data.Char as C
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+import Debug.Trace
+
+------
+iread :: String -> Int
+iread = read
+
+do2 f g x = (f x, g x)
+
+answer :: (Show a) => (String -> a) -> IO ()
+answer f = interact $ (++"\n") . show . f
+
+ord0 c = C.ord c - C.ord 'a'
+chr0 i = C.chr (i + C.ord 'a')
+incletter c i = chr0 ((ord0 c + i) `mod` 26)
+
+-- splitOn1 a b = fromJust $ stripInfix a b
+-- rsplitOn1 a b = fromJust $ stripInfixEnd a b
+
+-- pull out every part of a String that can be read in
+-- for some Read a and ignore the rest
+readOut :: Read a => String -> [a]
+readOut "" = []
+readOut s = case reads s of
+  [] -> readOut $ tail s
+  [(x, s')] -> x : readOut s'
+  _ -> error "ambiguous parse"
+ireadOut :: String -> [Int]
+ireadOut = readOut
+
+traceShow' s = traceShow s s
+
+--------
+
+
+parse lines = lines
+
+solve m = m
+
+main = answer $ solve . parse . lines
